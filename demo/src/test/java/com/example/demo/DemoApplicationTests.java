ddcb0108c36898b3941b1f9e0a25ad5e7c9e1fd7
peterparker <spidey.pp.earth616@gmail.com>
2025-10-29 10:01:09 +0530

    updated

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.data.mongodb.uri=mongodb://localhost:27017/testdb",
+    "spring.data.mongodb.auto-index-creation=true"
+})
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
